1e4b61f143cb6a06104194def268827995165d78
James <cnshoit@gmail.com>
2024-03-11 01:14:58 +0800

    fix lib test

--- projects/angular2-signaturepad/src/test.ts
+++ projects/angular2-signaturepad/src/test.ts
@@ -2,14 +2,20 @@
 
 import 'zone.js';
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
+import { getTestBed } from '@angular/core/testing';
+
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp,
+  ): {
     keys(): string[];
     <T>(id: string): T;
   };
@@ -18,11 +24,15 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+  platformBrowserDynamicTesting(),
+  {
+    teardown: { destroyAfterEach: false },
+  },
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = (import.meta as any).webpackContext('./', {
+  recursive: true,
+  regExp: /\.spec\.ts$/,
+});
 // And load the modules.
 context.keys().map(context);
